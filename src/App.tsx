ea9c7bf0283bce57925fea3a2910f797c5062f88
Benjamin Coullie <ben.coullie@vendhq.com>
2018-05-04 14:58:20 +1200

    testing changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
 import './App.css';
+
+import * as React from 'react';
 
 import logo from './logo.svg';
 
@@ -9,7 +10,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Gucci Oncall</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
